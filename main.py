97413ffadb3972d2e6fdb72377aa37c081aa7a58
Jose Diaz de Grenu de Pedro <jose.diaz@bosonit.com>
2021-10-31 20:50:55 +0100

    main: add alternative main method to test new data model
    This method, based on Events instead of transaction is superior for several
    reasons:
    * All swaps gets logged, even if not interacting with the router contract
    * For multipair trades, an Event is generated for each pair
    * Actual trade information (amounts of tokens and WBNB) are directly found
    
    This commit implements a basic method just for testing. This will be
    refactorized to gather all pairs and swaps from the blockchain, and keep it
    updated.
    
    Signed-off-by: Jose Diaz de Grenu de Pedro <jose.diaz@bosonit.com>

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from web3.types import BlockData
 
 from ParsedTx import ParsedTx
-from web3_utils import get_w3
+from data_models import DexTradePair, get_token, get_tx, get_DexTrade, DecentralizedExchange
+from web3_utils import get_w3, get_contract, WBNB_ADDRESS
 
 
 def process_block(block_data: BlockData):
@@ -9,6 +10,34 @@ def process_block(block_data: BlockData):
         parsed_tx = ParsedTx(tx)
         if parsed_tx.is_simple_wbnb_tx():
             print(parsed_tx)
+
+
+def main_events():
+    w3 = get_w3()
+    last_block = w3.eth.get_block_number()
+
+    for dex in DecentralizedExchange:
+        contract = get_contract(w3, dex.factory_addr)
+        new_pairs_filter = contract.events.PairCreated.createFilter(fromBlock=last_block-5000, toBlock=last_block)
+        for pair_created in new_pairs_filter.get_all_entries():
+            print(pair_created)
+            pair_contract = get_contract(w3, pair_created.args['pair'])
+            if WBNB_ADDRESS not in (pair_created.args.values()):
+                print(f"Ignoring not-WBNB pair: {pair_created.args}")
+                continue
+
+
+            is_token0_wbnb = WBNB_ADDRESS == pair_created.args.token0
+            token_addr = pair_created.args.token1 if is_token0_wbnb else pair_created.args.token0
+            token = get_token(token_addr)
+
+            pair = DexTradePair(dex, token, get_tx(pair_created.transactionHash), is_token0_wbnb)
+
+            swaps_filter = pair_contract.events.Swap.createFilter(fromBlock=last_block-5000, toBlock=last_block)
+            for swap in swaps_filter.get_all_entries():
+                print(get_DexTrade(swap, pair))
+        print(contract)
+
 
 def main():
     w3 = get_w3()
@@ -24,4 +53,5 @@ def main():
 
 
 if __name__ == '__main__':
+    main_events()
     main()
