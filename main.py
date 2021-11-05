c0104859150bcd88674c9891ea6af00522ce9da5
Jose Diaz de Grenu de Pedro <jose.diaz@bosonit.com>
2021-11-05 22:51:43 +0100

    main: add a couple of functions to simplify main()
    Signed-off-by: Jose Diaz de Grenu de Pedro <jose.diaz@bosonit.com>

--- main.py
+++ main.py
@@ -1,17 +1,72 @@
 import itertools
 import os
 import time
+from typing import Dict
 
-from data_models import DecentralizedExchange
+from web3 import Web3
+from web3.contract import Contract
+
+from data_models import DecentralizedExchange, DecentralizedExchangeType, DexTradePair
 from ddbb_manager import DDBBManager
 from entity_factory import EntityFactory
-from web3_utils import get_w3, get_contract
-
-
+from web3_utils import get_w3, get_contract, get_lptoken_contract
 
 BLOCK_FOR_THE_FIRST_LP = 6810423
 BLOCK_LENGTH = 5000
 
+def get_new_pairs(
+        dex_factories: Dict[DecentralizedExchangeType, Contract],
+        start_block: int,
+        e_factory: EntityFactory
+) -> Dict[DexTradePair, Contract]:
+    new_pairs = {}
+    for dex, factory_contract in dex_factories.items():
+        for retry in itertools.count():
+            try:
+                print(f"\tGetting pairs for {dex.dex_name}...")
+                new_pairs_filter = factory_contract.events.PairCreated.createFilter(
+                    fromBlock=start_block - 1,
+                    toBlock=start_block + BLOCK_LENGTH - 1
+                )
+
+                for pair_created in new_pairs_filter.get_all_entries():
+                    pair = e_factory.get_DexTradePair(dex, pair_created)
+                    if not pair:
+                        continue
+
+                    pair_contract = get_contract(get_w3(), pair_created.args['pair'])
+                    new_pairs[pair] = pair_contract
+                break
+            except (Exception,) as e:
+                if retry > 1:
+                    print(f"ERROR (retry #{retry}): {e}")
+
+    return new_pairs
+
+def find_and_persist_trades(
+        pair: DexTradePair,
+        pair_contract: Contract,
+        start_block: int,
+        e_factory: EntityFactory,
+        ddbb_manager: DDBBManager
+) -> int:
+    for retry in itertools.count():
+        try:
+            swaps_filter = pair_contract.events.Swap.createFilter(
+                fromBlock=start_block - 1,
+                toBlock=start_block + BLOCK_LENGTH - 1
+            )
+
+            # Iterate through .get_all_entries() fast, otherwise the Web3 provider will forget about our
+            # filter id, and we won't be able to finish to iterate the swap entries (exception will be raisen)
+            swaps = [swap for swap in swaps_filter.get_all_entries()]
+            for swap in swaps:
+                ddbb_manager.persist(e_factory.get_DexTrade(swap, pair))
+
+            return len(swaps)
+        except (Exception,) as e:
+            if retry > 1:
+                print(f"ERROR (retry #{retry}): {e}")
 
 def main():
     db_manager = DDBBManager(os.getenv("DDBB_STRING"))
@@ -26,7 +81,7 @@ def main():
     }
     print("Reading pairs...")
     pairs = {
-        pair: get_contract(w3, pair.pair_addr)
+        pair: get_lptoken_contract(w3, Web3.toChecksumAddress(pair.pair_addr))
         for pair in db_manager.get_all_pairs()
     }
     print("Done!")
@@ -37,54 +92,22 @@ def main():
     block = start_block
     while True:
         print(f"Import blocks {block}-{block + BLOCK_LENGTH}...")
-        # Find new pairs
-        entities_to_persist = []
-        for dex, factory_contract in dex_factories.items():
-            for retry in itertools.count():
-                try:
-                    print(f"\tGetting pairs for {dex.dex_name}...", end="",
-                          flush=True)
-                    new_pairs_filter = factory_contract.events.PairCreated.createFilter(
-                        fromBlock=block - 1,
-                        toBlock=block + BLOCK_LENGTH - 1
-                    )
 
-                    pairs_found = 0
-                    for pair_created in new_pairs_filter.get_all_entries():
-                        pair = e_factory.get_DexTradePair(dex, pair_created)
-                        if not pair:
-                            continue
+        new_pairs = get_new_pairs(dex_factories, block, e_factory)
+        if len(new_pairs) > 0:
+            print(f"\tGot {len(new_pairs)} new pairs")
+            pairs.update(new_pairs)
 
-                        pair_contract = get_contract(w3, pair_created.args['pair'])
-                        pairs[pair] = pair_contract
-                        entities_to_persist.append(pair)
-                        pairs_found += 1
-                    print(f" Found {pairs_found} pairs")
-                    break
-                except (Exception, ) as e:
-                    if retry > 1:
-                        print(f"ERROR (retry #{retry}): {e}")
-
-        # Find new swaps for existing pairs
         print("\tLooking for swaps...")
-        for pair, pair_contract in pairs.items():
-            for retry in itertools.count():
-                try:
-                    swaps_filter = pair_contract.events.Swap.createFilter(
-                        fromBlock=block - 1,
-                        toBlock=block + BLOCK_LENGTH - 1
-                    )
-
-                    # Iterate through .get_all_entries() fast, otherwise the Web3 provider will forget about our
-                    # filter id, and we won't be able to finish to iterate the swap entries (exception will be raisen)
-                    swaps = [swap for swap in swaps_filter.get_all_entries()]
-                    swaps = [e_factory.get_DexTrade(swap, pair) for swap in swaps]
-
-                    entities_to_persist.extend(swaps)
-                    break
-                except (Exception, ) as e:
-                    if retry > 1:
-                        print(f"ERROR (retry #{retry}): {e}")
+        trades_found = sum(
+            find_and_persist_trades(
+                pair,
+                pair_contract,
+                block,
+                e_factory,
+                db_manager
+            ) for pair, pair_contract in pairs.items()
+        )
 
         seconds_so_far = time.time() - start_time
         blocks_processed = block + BLOCK_LENGTH - start_block
@@ -95,22 +118,18 @@ def main():
         print(
             f"Processed blocks {block}-{block + BLOCK_LENGTH} "
             f"({blocks_per_second:.2f} block/second, {remaining_seconds / 3600:.2f} hours remaining)")
-        print(f"\tPairs: {len(pairs)}")
-        print(f"\tSwaps: {len(entities_to_persist)}")
+        print(f"\tTotal pairs: {len(pairs)}")
+        print(f"\tNew swaps: {trades_found}")
         print(f"")
         block += BLOCK_LENGTH
 
-        if entities_to_persist:
-            print(f"Persisting...")
-            try:
-                start_persist_time = time.time()
-                db_manager.persist(entities_to_persist)
-                print(f" {len(entities_to_persist)} persisted in {time.time() - start_persist_time} seconds!")
-                entities_to_persist.clear()
-            except (Exception,) as e:
-                print(f" Error persisting: {e}")
+        try:
+            start_persist_time = time.time()
+            db_manager.commit_changes()
+            print(f"New entities commited in {time.time() - start_persist_time} seconds!")
+        except (Exception,) as e:
+            print(f"Error committing: {e}")
 
-        db_manager.commit_changes()
 
 
 if __name__ == '__main__':
