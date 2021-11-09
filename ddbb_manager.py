3916f227105f025f5e364588fe64aed3ebd884fa
Jose Diaz de Grenu de Pedro <jose.diaz@bosonit.com>
2021-11-09 21:37:27 +0100

    ddbb_manager: rework multithread support
    Instead of reinventing the wheel with Queue's and Lock's, just use a single
    thread ThreadPoolExecutor. The thread managed by this executor should be
    the only one accessing self.__session.
    
    Note that in the previous implementation, self.__session was used by
    multiple threads (even though never concurrently) which is incorrect.
    
    Signed-off-by: Jose Diaz de Grenu de Pedro <jose.diaz@bosonit.com>

--- ddbb_manager.py
+++ ddbb_manager.py
@@ -1,10 +1,8 @@
 import logging
-from queue import Queue
-from threading import Thread, Lock
-from typing import List, Any, Callable
+from concurrent.futures import ThreadPoolExecutor
+from typing import List
 
 from sqlalchemy import create_engine, text, desc
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker, Session, joinedload
 
 from data_models import mapper_registry, Block, DexTradePair
@@ -17,73 +15,54 @@ class DDBBManager:
         self.ddbb_string = ddbb_string
         self.__engine = self.__create_ddbb_engine(ddbb_string, prune_schema=prune_schema)
         self.__session: Session = sessionmaker(bind=self.__engine)()
-        self.__work_queue = Queue()
-        self.__session_lock = Lock()
-        self.__worker_thread = Thread(name="DDBBManagerWorker", target=self.__worker_thread)
+        self.__executor = ThreadPoolExecutor(max_workers=1, thread_name_prefix='DDBBManagerWorker')
 
         if not self.__engine:
             raise ValueError("could not create DDBB engine")
 
-        self.__worker_thread.start()
-
-
-    def __session_work(self, work: Callable[[Session], Any], error_msg: str = ""):
-        with self.__session_lock:
-            try:
-                return work(self.__session)
-            except SQLAlchemyError:
-                DDBBManager.logger.exception(f"{error_msg}")
-
-    def __worker_thread(self):
-        while True:
-            entity = self.__work_queue.get()
-            self.__session_work(lambda s: s.merge(entity), "Inserting entity")
 
     def __str__(self):
         return f"DDBBManager<{self.__engine}>"
 
     def get_last_block(self) -> Block:
-        block_list = self.__session_work(
-            lambda s: s \
+        block_list = self.__executor.submit(
+            lambda: self.__session \
                 .query(Block) \
                 .order_by(desc(Block.number)) \
                 .limit(1).all()
-        )
-
+        ).result()
 
         return block_list[0] if block_list else None
 
     def get_all_pairs(self) -> List[DexTradePair]:
-        return self.__session_work(
-            lambda s: s \
+        return self.__executor.submit(
+            lambda: self.__session \
                 .query(DexTradePair) \
                 .options(joinedload('*')) \
-                .all(),
-            "Getting all pairs"
-        )
+                .all()
+        ).result()
 
     def get_entity_by_pl(self, cls, primary_key_value):
-        return self.__session_work(
-            lambda s: s.query(cls).get(primary_key_value),
-            "Getting entity by pk"
+        return self.__executor.submit(
+            lambda: self.__session.query(cls).get(primary_key_value)
+        ).result()
+
+    def commit_changes(self, sync=False):
+        f = self.__executor.submit(
+            lambda: self.__session.commit()
         )
 
-    def commit_changes(self):
-        def __work(session):
-            if self.__work_queue.qsize() > 0:
-                return False
+        if sync:
+            f.result()
 
-            session.commit()
-            return True
 
-        while not self.__session_work(
-            lambda s: __work(s),
-            "Comiting changes"
-        ):
-            pass # Try again
+    def persist(self, entity, sync=False) -> None:
+        f = self.__executor.submit(
+            lambda: self.__session.merge(entity)
+        )
 
-    def persist(self, entity) -> None:
-        self.__work_queue.put(entity)
+        if sync:
+            f.result()
 
     @staticmethod
     def __create_ddbb_engine(ddbb_string, prune_schema=False):
